72f4c5c112af18eeb42e9f23ebc38fc073e1ecd9
Erik Tank <erik@jundy.com>
2014-01-21 17:16:26 -0700

    Correct needed services list.
    Just because the service hasn't been done before doesn't mean it needs
    to be done yet.

--- Auto/User/Car/Service.pm
+++ Auto/User/Car/Service.pm
@@ -7,8 +7,8 @@ __PACKAGE__->columns(All => qw(id user_car_id service_id));
 __PACKAGE__->has_a(user_car_id => 'Auto::User::Car');
 __PACKAGE__->has_a(service_id  => 'Auto::Service');
 
-sub car     { return shift->car_id;     }
-sub service { return shift->service_id; }
+sub user_car { return shift->user_car_id; }
+sub service  { return shift->service_id;  }
 
 sub needed {
   my $class = shift;
@@ -24,12 +24,8 @@ sub needed {
       { order_by => 'mileage' }
     );
 
-    if ($maint) {
-      if ($maint->user_car->mileage - $maint->mileage >= $user_car_service->service->miles) {
-        push(@needed, $user_car_service);
-      }
-    }
-    else {
+    my $maint_mileage = $maint ? $maint->mileage : 0;
+    if ($user_car_service->user_car->mileage - $maint_mileage >= $user_car_service->service->miles) {
       push(@needed, $user_car_service);
     }
   }
